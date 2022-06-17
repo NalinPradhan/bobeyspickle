12c469b5f3111e2986620d435b728a010e29613a
NalinPradhan <pnalin63@gmail.com>
2022-06-17 15:54:18 +0530

    fourth

--- pickleweb/settings.py
+++ pickleweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9^wo_!fbu=h8zu1^2*x(2xc-@#_v&rs&6qkbx@s@g_#!12vtv&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bobeyspickle.com','bobeyspickle.herokuapp.com']
 
 
 # Application definition
