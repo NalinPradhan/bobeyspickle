e02f106af4820e8a16ce5236ccafb54080dc8d05
NalinPradhan <pnalin63@gmail.com>
2022-06-17 20:05:03 +0530

    nothinf

--- pickleweb/settings.py
+++ pickleweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9^wo_!fbu=h8zu1^2*x(2xc-@#_v&rs&6qkbx@s@g_#!12vtv&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['bobeyspickle.com','bobeyspickle.herokuapp.com','127.0.0.1']
 
@@ -116,7 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
  
 STATIC_URL = 'static/'
-STATICFILES_DIRS = None
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
